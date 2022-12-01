efd52325a08fdd1feda2a57c2dc412234fa96699
Serhii Tkachenko <doker.tk@gmail.com>
2022-12-01 22:52:13 +0100

    fix

--- Users CoreData/Sources/Modules/Detail/DetailViewController.swift
+++ Users CoreData/Sources/Modules/Detail/DetailViewController.swift
@@ -11,8 +11,6 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
 }
